1b0882fcb66ca91c351b03194d499bcf982c0006
Valeria Santos <81273050+Valeriaaparecida@users.noreply.github.com>
2023-09-19 22:50:49 -0300

    criação do modelo de dados do feedback

---
+++ feedbacks-api-barbeshop/Models/Feedback.cs
@@ -0,0 +1,27 @@
+﻿
+ using System;
+ using System.ComponentModel.DataAnnotations;
+
+   namespace feedbacks_api_barbeshop.Models
+{
+        public class Feedback
+        {
+            public int Id { get; set; }
+
+            [Required]
+            public int ClienteId { get; set; }
+
+            [Required]
+            public int ServicoId { get; set; }
+
+            [Required]
+            [Range(1, 5)]
+            public int Classificacao { get; set; }
+
+            [Required]
+            public string Comentarios { get; set; }
+
+            [Required]
+            public DateTime DataFeedback { get; set; }
+        }
+    }
